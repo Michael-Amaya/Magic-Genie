db202e97ca23f567c68144047acff991a26a5de9
Fabrizzio Perez <fabrizzio46@gmail.com>
2023-05-11 00:07:23 -0400

    make sure length of text area is greater than 4

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,12 @@
 window.onsubmit = () => {
 
-    return confirm('Are you sure you want to ask this question to the genie?')
+    let question = document.getElementById("question").value;
+
+    if(question.length < 4){
+        alert("Did you even ask a question???");
+        return false;
+    }
+
+    return confirm('Are you sure you want to ask this question to the genie?');
 
 }
